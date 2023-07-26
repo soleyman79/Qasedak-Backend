e12092e14417c9f6563df23d9249ad164616704b
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-07-26 15:28:31 +0330

    Update settings

--- qasedak/settings.py
+++ qasedak/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n0-d%)y$t1@sb(d+47m48z$2469uji!ybehtm^r3ck5pn@$u8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
