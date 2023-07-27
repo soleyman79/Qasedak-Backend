162971c8a377304906f8abffa5f90defd8e26993
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-07-27 09:01:53 +0330

    Change db

--- qasedak/settings.py
+++ qasedak/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'qasedak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    }
+}
 
 
 # Password validation
