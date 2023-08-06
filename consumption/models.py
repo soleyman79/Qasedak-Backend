b60e5ee6e3cf0bfd02f318388b1c4610932712bd
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-08-06 12:26:57 +0330

    Comment models

--- consumption/models.py
+++ consumption/models.py
@@ -3,24 +3,24 @@ from django.core.validators import FileExtensionValidator
 
 
 
-class Content(models.Model):
-    title = models.CharField(max_length=64)
-    summary = models.CharField(max_length=64)
-    chanel = models.ForeignKey('Chanel', on_delete=models.CASCADE)
+# class Content(models.Model):
+#     title = models.CharField(max_length=64)
+#     summary = models.CharField(max_length=64)
+#     chanel = models.ForeignKey('Chanel', on_delete=models.CASCADE)
     
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
 
-class Video(Content):
-    video = models.FileField(upload_to='videos_uploaded', 
-                             null=False, 
-                             validators=[FileExtensionValidator(allowed_extensions=['avi','mp4','webm','mkv'])])
+# class Video(Content):
+#     video = models.FileField(upload_to='videos_uploaded', 
+#                              null=False, 
+#                              validators=[FileExtensionValidator(allowed_extensions=['avi','mp4','webm','mkv'])])
 
 
-class Photo(Content):
-    photo = models.ImageField(upload_to='photos_uploaded')
+# class Photo(Content):
+#     photo = models.ImageField(upload_to='photos_uploaded')
     
 
-class Chanel(models.Model):
-    name = models.CharField(unique=True, max_length=128)
+# class Chanel(models.Model):
+#     name = models.CharField(unique=True, max_length=128)
