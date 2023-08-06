c8f284e5cbc4c9e1440b5ad1b8245b942be38c9e
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-08-06 21:20:56 +0330

    Update auth

--- consumption/views.py
+++ consumption/views.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponse, JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from creation.models import Member, Chanel
 from users.models import Session
 
 
 
+@csrf_exempt
 def userChannels(request):
     if request.method == 'POST':
         session = request.POST['session']
         if not Session.objects.filter(session=session).exists():
             return HttpResponse('Login First')
         
-        user = Session.objects.get(session=session)
+        user = Session.objects.get(session=session).user
         channelsId = Member.objects.filter(user=user).values_list('chanel', flat=True)
         channels = Chanel.objects.filter(id__in=channelsId)
         return JsonResponse({'names': list(channels.values_list('name', flat=True))})
--- creation/views.py
+++ creation/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
 from users.models import Session
